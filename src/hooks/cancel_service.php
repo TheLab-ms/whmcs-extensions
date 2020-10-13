05cf173cf6ecfad64f453b8e342ac6e27ed658b9
Tommy Falgout <tommy@lastcoolnameleft.com>
2020-10-13 12:34:04 -0500

    add cancel service hook

---
+++ src/hooks/cancel_service.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once  __DIR__ . '/whmcs_controller.php';
+
+# https://developers.whmcs.com/hooks-reference/client/
+# For a quick test, change to "ClientEdit" and you can edit the client instead of creating a new one.  Parameters are the same.
+add_hook('CancellationRequest', 1, function($vars) {
+    require __DIR__ . '/config.php';
+    logActivity('Cancellation Request: ' . json_encode($vars), 0);
+    $client_id  = $vars['userid'];
+    $service_id = $vars['relid'];
+    $reason     = $vars['reason'];
+    $type       = $vars['type'];
+
+    $subject = "Member cancelled.  Delete Badge ID for Client $client_id";
+    $body = "Copy the Badge #: https://{$_SERVER['HTTP_HOST']}/admin/clientsprofile.php?userid=$client_id\n"
+        . "Log into $BADGE_ACS_LOGIN_ENDPOINT, search $client_id\n"
+        . "Click Users\n"
+        . "Search for Badge # from previous page\n"
+        . "Click Delete\n"
+        . "Click OK";
+    open_ticket($subject, $body);
+});
