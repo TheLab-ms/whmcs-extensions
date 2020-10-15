508d0210ee48603a3c93f36c0b1266278e15378a
Tommy Falgout <tommy@lastcoolnameleft.com>
2020-10-14 22:59:03 -0500

    add logo asset

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #/bin/sh
 
-sudo rm -rf /data/whmcs-staging/website/includes/hooks/*
-sudo cp src/hooks/* /data/whmcs-staging/website/includes/hooks/
-sudo chown -R www-data:www-data /data/whmcs-staging/website/includes/hooks/
+# Example usage:
+# ./install
+# OR 
+# BASE_DIR=/data/whmcs ./install.sh
+
+BASE_DIR=${BASE_DIR:-/data/whmcs-staging}
+
+sudo rm -rf $BASE_DIR/website/includes/hooks/*
+sudo cp src/hooks/* $BASE_DIR/website/includes/hooks/
+sudo chown -R www-data:www-data $BASE_DIR/website/includes/hooks/
+sudo cp assets/img/logo.png $BASE_DIR/website/assets/img/logo.png
+sudo chown -R www-data:www-data $BASE_DIR/website/assets/img
